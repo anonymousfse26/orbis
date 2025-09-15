ec878c7727fd97563148e587ee52d88a9ccdf46a
anonymousfse26 <minjong.kim@skku.edu>
2025-09-15 14:30:54 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,26 @@
 from setuptools import find_packages
 from setuptools import setup
 
-from scope import __version__
+from orbis import __version__
 
 # with open('./README.md') as f:
 #     LONG_DESCRIPTION = f.read()
 
 setup(
-    name='scope',
+    name='orbis',
     version=__version__,
-    description='SCOPE: Enhancing Symbolic Execution through Optimized Option-Related Branch Exploration',
-    # long_description=LONG_DESCRIPTION,
+    description='ORBiS: Enhancing Symbolic Execution through Optimized Option-Related Branch Exploration',
     python_version='>=3.6',
-    packages=find_packages(include=('scope', 'scope.*')),
+    packages=find_packages(include=('orbis', 'orbis.*')),
     include_package_data=True,
     setup_requires=[],
     install_requires=[
-        'numpy',
-        'tree-sitter<=0.21.3',
-        'clang',
-        'scikit-learn',
+        'numpy'
     ],
     dependency_links=[],
     entry_points={
         'console_scripts': [
-            'scope=scope.bin:main',
+            'orbis=orbis.bin:main',
         ]
     }
 )
